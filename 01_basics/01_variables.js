53bd21cc406ae71f3ff91a3f29a6f8716a3f580c
Subhodh96 <145917409+Subhodh96@users.noreply.github.com>
2025-02-25 16:26:37 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345
+let accountEmail = "subh@gmail.com"
+var accountPassword = "212121"
+accountCity = "jaipur"
+
+accountEmail = "monu@gmail.com"
+accountPassword = "798696"
+accountCity = "indore"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
