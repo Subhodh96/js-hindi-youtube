01b86ee905105f8cd1fc1caa1a5fcd213da3faef
Subhodh96 <145917409+Subhodh96@users.noreply.github.com>
2025-04-13 17:06:16 +0000

    loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,32 @@
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+   // console.log(element);
+    
+}
+
+let myArray = ["flash", "spiderman", "batman"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element);
+    
+}
+
+// for (let i = 0; i <=20; i++) {
+//     if (i == 5) {
+//         console.log(`detected 5`);
+//         break
+//     }
+//     console.log(`value of i is ${i}`);
+    
+    
+// }
+
+for (let i = 0; i <=20; i++) {
+    if (i == 5) {
+        console.log(`detected 5`);
+        continue
+    }
+    console.log(`value of i is ${i}`);
+    
+    
+}
